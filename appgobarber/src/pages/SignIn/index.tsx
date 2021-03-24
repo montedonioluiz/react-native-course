c227151585a6943efb95fae07e6c190016f6c419
Luiz Oliveira Montedonio <montedonio.luiz@gmail.com>
2021-03-24 13:47:51 -0300

    layout sign in

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 // Components
 import { Image } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 // Styles
 import { Container, Title } from './styles';
@@ -12,7 +14,13 @@ import logoImg from '../../assets/logo.png'
 const SignIn: React.FC = () => {
   return <Container >
     <Image source={logoImg} />
+
     <Title>Faça seu Logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail"/>
+    <Input name="password" icon="lock" placeholder="Senha"/>
+
+    <Button onPress={() => {console.log("a")}}>Entrar</Button>
   </Container>
 }
 
--- appgobarber/src/pages/SignIn/styles.ts
+++ appgobarber/src/pages/SignIn/styles.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  padding: 0 30px;
+  
   align-items: center;
   justify-content: center;
+
 `;
 
 export const Title = styled.Text`
