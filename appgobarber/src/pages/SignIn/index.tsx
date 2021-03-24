590398aca10d5650eb4e31b7b92a2e6015ec5724
Luiz Oliveira Montedonio <montedonio.luiz@gmail.com>
2021-03-24 14:19:29 -0300

    finish signUp screen

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,27 +1,56 @@
 import React from 'react';
 
 // Components
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+// Icon
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 // Styles
-import { Container, Title } from './styles';
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 
 // Assets
 import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
-  return <Container >
-    <Image source={logoImg} />
+  return <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Image source={logoImg} />
 
-    <Title>Faça seu Logon</Title>
+          <View>
+            <Title>Faça seu Logon</Title>
+          </View>
 
-    <Input name="email" icon="mail" placeholder="E-mail"/>
-    <Input name="password" icon="lock" placeholder="Senha"/>
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
 
-    <Button onPress={() => {console.log("a")}}>Entrar</Button>
-  </Container>
+          <Button onPress={() => { console.log("a") }}>Entrar</Button>
+
+          <ForgotPassword>
+            <ForgotPasswordText>
+              Esqueci minha senha
+      </ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <CreateAccountButton onPress={() => { }} >
+      <FeatherIcon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+    </CreateAccountButton>
+  </>
 }
 
 export default SignIn;
